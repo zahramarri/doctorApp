669a28ecb6a8d232f8505bc2ae8b5ac79a3fd1a2
zahramarri <zahramarri.dev@gmail.com>
2022-06-13 18:32:31 +0430

    set content view

--- app/src/main/java/com/learning/doctorapp/MainActivity.kt
+++ app/src/main/java/com/learning/doctorapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.learning.doctorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.learning.doctorapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
