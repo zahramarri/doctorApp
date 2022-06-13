e67523537e37ed2edcbfc1688ff791f065737c30
zahramarri <zahramarri.dev@gmail.com>
2022-06-14 00:47:54 +0430

    add some code to navigate from main activity to activity 2

--- app/src/main/java/com/learning/doctorapp/MainActivity.kt
+++ app/src/main/java/com/learning/doctorapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.learning.doctorapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.learning.doctorapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonDoctor1.setOnClickListener(::goToActivity2)
+    }
+
+    private fun goToActivity2(view: View) {
+        val intent = Intent(this, Activity2::class.java)
+        startActivity(intent)
     }
 }
