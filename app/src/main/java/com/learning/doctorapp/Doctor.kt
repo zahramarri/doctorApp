bfba80cc8bdaa6060e5d7d91e33fb7576742c528
zahramarri <zahramarri.dev@gmail.com>
2022-06-16 18:20:25 +0430

    implement class Serializable

--- app/src/main/java/com/learning/doctorapp/Doctor.kt
+++ app/src/main/java/com/learning/doctorapp/Doctor.kt
@@ -1,6 +1,8 @@
 package com.learning.doctorapp
 
+import java.io.Serializable
+
 data class Doctor(val iD: Int,
                   val name: String,
                   val address: String,
-                  val phoneNumber: String)
+                  val phoneNumber: String): Serializable
