922b2312add485b3e09aa957e3200f1a565b7e02
zahramarri <zahramarri.dev@gmail.com>
2022-06-17 12:19:37 +0430

    set view binding for doctor info fragment

--- app/src/main/java/com/learning/doctorapp/DoctorInfoFragment.kt
+++ app/src/main/java/com/learning/doctorapp/DoctorInfoFragment.kt
@@ -5,18 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.learning.doctorapp.databinding.FragmentDoctorInfoBinding
 
 class DoctorInfoFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var binding: FragmentDoctorInfoBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_doctor_info, container, false)
+    ): View {
+        binding = FragmentDoctorInfoBinding.inflate(inflater)
+        return binding.root
     }
 }
