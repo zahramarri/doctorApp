12be4f9dc505304a9d9207c7de4115f35afd44b6
zahramarri <zahramarri.dev@gmail.com>
2022-06-17 14:47:39 +0430

    add fragment to activity 2 layout for test

--- app/src/main/java/com/learning/doctorapp/DoctorInfoFragment.kt
+++ app/src/main/java/com/learning/doctorapp/DoctorInfoFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.learning.doctorapp.databinding.FragmentDoctorInfoBinding
 
 class DoctorInfoFragment : Fragment() {
@@ -16,7 +17,10 @@ class DoctorInfoFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentDoctorInfoBinding.inflate(inflater)
+        binding = FragmentDoctorInfoBinding.inflate(inflater, container, false)
+        binding.buttonFragmentTest.setOnClickListener {
+            Toast.makeText(activity, "clicked", Toast.LENGTH_SHORT).show()
+        }
         return binding.root
     }
 }
